c6b851217ceaeb34c5ad6a87e178d07e7714044f
Zhaofeng Liang <zhaofengliang@utexas.edu>
2017-04-22 17:36:25 -0500

    Add files via upload

--- src/assignment7/ServerMain.java
+++ src/assignment7/ServerMain.java
@@ -1,27 +1,56 @@
 package assignment7;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
 
-public class ServerMain extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("ChatRoom.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("Chat Room");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+public class ServerMain extends Observable {
 
-    public static void main(String args[]){
-        launch();
-    }
+	public static void main(String[] args) {
+		try {
+			new ServerMain().setUpNetworking();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		ServerSocket serverSock = new ServerSocket(4242);
+		while (true) {
+			Socket clientSocket = serverSock.accept();
+			ClientObserver writer = new ClientObserver(clientSocket.getOutputStream());
+			Thread t = new Thread(new ClientHandler(clientSocket));
+			t.start();
+			this.addObserver(writer);
+			System.out.println("got a connection");
+		}
+	}
+
+	class ClientHandler implements Runnable {
+		private BufferedReader reader;
+
+		public ClientHandler(Socket clientSocket) {
+			Socket sock = clientSocket;
+			try {
+				reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					System.out.println("server read " + message);
+					setChanged();
+					notifyObservers(message);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
