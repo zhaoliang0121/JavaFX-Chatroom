e87d88d0c9eb7e86e2fb5ac196fc0d22bbf83b89
Zohaib <kavzoh@utexas.edu>
2017-04-22 16:05:15 -0500

    Structuring Server

---
+++ src/assignment7/ServerMain.java
@@ -0,0 +1,27 @@
+package assignment7;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ServerMain extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ChatRoom.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Chat Room");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]){
+        launch();
+    }
+}
